93da581a486cd38bd8ee4ba8b287161cddcac3cf
SuleymanKahraman <suleymankahraman1993@gmail.com>
2023-03-12 16:49:55 +0300

    client ve api haberleşti

---
+++ InspectorPortal.Common/Dtos/AuthenticationRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace InspectorPortal.Common.Dtos
+{
+    public class AuthenticationRequestDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ InspectorPortal/Controllers/AuthenticationController.cs
@@ -0,0 +1,30 @@
+﻿using InspectorPortal.Common.Dtos;
+using InspectorPortal.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InspectorPortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly InspectorPortalDbContext dbContext;
+
+        public AuthenticationController(InspectorPortalDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] AuthenticationRequestDto input)
+        {
+            var entity = dbContext.Users.FirstOrDefault(x=>x.Email == input.Email && x.Password == input.Password);
+            if(entity != null)
+            {
+                return Ok(entity.FirstName);
+            }
+            return BadRequest();
+            //TODO: handle
+        }
+    }
+}
