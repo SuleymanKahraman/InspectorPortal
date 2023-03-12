71f7afa279930230a0fd4458739ca30cd44744fb
SuleymanKahraman <suleymankahraman1993@gmail.com>
2023-03-12 14:24:03 +0300

    Add project files.

---
+++ InspectorPortal.Data/Entities/User.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InspectorPortal.Data.Entities
+{
+    [Table("Users")] //kaldırılabilir.
+    public class User
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(64)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(64)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        public string Password { get; set; }
+
+        //public int UserDetailId { get; set; }
+
+        //[ForeignKey(nameof(UserDetailId))]
+        //public virtual UserDetail UserDetail { get; set; }
+    }
+}
---
+++ InspectorPortal/Controllers/AuthenticationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InspectorPortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login()
+        {
+            //TODO: handle
+            return Ok();
+        }
+    }
+}
